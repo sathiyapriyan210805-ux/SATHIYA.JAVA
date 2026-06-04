d5bf341e27ffcd26809185eee812a046b8d3143f
SATHIYAPRIYAN <sathiyapriyan210805@gmail.com>
2026-06-04 10:24:05 +0530

    Add files via upload

---
+++ Remove.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package remove;
+import java.util.ArrayList;
+/**
+ *
+ * @author sp0805
+ */
+public class Remove {
+
+     public static void main(String[] args) {
+     
+        ArrayList<String> list = new ArrayList<>();
+        list.add("java");
+        list.add("python");
+        list.remove(1);
+        System.out.println(list);
+    }
+    
+}
